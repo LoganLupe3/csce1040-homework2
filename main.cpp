739dda11ebbe8a4bd43db7e483b43b5216e87c49
loganlupe3 <loganlupeamanu@my.unt.edu>
2021-10-26 14:09:33 -0500

    Added test loan to main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include "Customer.h"
+#include "Movie.h"
+#include "Loan.h"
 
 using namespace std;
 
@@ -8,4 +10,8 @@ int main(){
   Customer* testCustomer = new Customer(123456, 123456123456, "Logan", 9032001, 123, 2);
 
   cout << testCustomer->GetName() << " has " << testCustomer->GetNumMovies() << " movies out." << endl;
+
+  Loan* testLoan = new Loan(12345678, 123, 123, 1231, 2341, Out);
+
+  cout << "Loan ID: " << testLoan->GetID() << endl;
 }
