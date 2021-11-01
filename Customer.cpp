de128d70976c2e787d8fb726c0236eb94e8a6bf3
LoganLupe3 <76707558+LoganLupe3@users.noreply.github.com>
2021-11-01 17:42:50 -0500

    Updated comments to portray project board better

--- Customer.cpp
+++ Customer.cpp
@@ -8,7 +8,7 @@ Customer::Customer(){
   id = 0;
   creditCard = 0;
   name = "";
-  expirationDate = 0;
+  expirationDate = 0; // This needs to be split into 3 different integers: day, month, year
   key = 0;
   numMovies = 0;
 }
@@ -17,11 +17,13 @@ Customer::Customer(int i, long long int c, string n, int exp, int k, int nm){
   id = i;
   creditCard = c;
   name = n;
-  expirationDate = exp;
+  expirationDate = exp; // Refactor to day, month, year
   key = k;
   numMovies = nm;
 }
 
+// Delete GetExpiration()
+// Add GetDay(), GetMonth(), GetYear()
 int Customer::GetID(){ return id; }
 long long int Customer::GetCC(){ return creditCard; }
 string Customer::GetName(){ return name; }
@@ -29,6 +31,8 @@ int Customer::GetExpiration(){ return expirationDate; }
 int Customer::GetKey(){ return key; }
 int Customer::GetNumMovies(){ return numMovies; }
 
+// Delete SetExpiration(int)
+// Add SetDay(int), SetMonth(int), SetYear(int)
 void Customer::SetID(int i){ id = i; }
 void Customer::SetCC(long long int c){ creditCard = c; }
 void Customer::SetName(string n){ name = n; }
